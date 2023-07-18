7a49a385ccce876d34d3a09f820a2c8f484e5345
Mauricio Alvarez <maurialvarez103@gmail.com>
2023-07-18 11:07:19 -0300

    modifico componente card

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -13,9 +13,8 @@ import AddCartButton from "../AddCartButton/AddCartButton";
 const ItemListContainer = ({ product }) => {
   return (
     <div>
-      <Card sx={{ Width: 200, background: "#5c5c61", margin: 0}}>
-        <CardActionArea className="action">
-          <CardMedia sx={{borderRadius: 10,padding: 2, width: 270, height: 370, justifyContent: 'center'}} component="img" className="imgItem" src={product.image}  alt="" />
+      <Card sx={{ Width: 200, background: '#242427', margin: 0, border: '1px solid #a2b30bcf'}}>
+          <CardMedia sx={{borderRadius: 10,padding: 2, width: 270, height: 370, justifyContent: 'center',}} component="img" className="imgItem" src={product.image}  alt="" />
           <CardContent>
             <Typography sx={{color: "white"}} gutterBottom variant="h5" component="div">
               {product.name}
@@ -30,7 +29,6 @@ const ItemListContainer = ({ product }) => {
           </Link>
           <AddCartButton product={product}/>
         </div>
-        </CardActionArea>
       </Card>
     </div>
   )
